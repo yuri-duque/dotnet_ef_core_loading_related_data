8b889e485f12c1f4851ed511bda010d563ea5ccb
yuri-duque <yurithielmann83@gmail.com>
2023-03-21 23:16:33 -0300

    creating a workout class

---
+++ Domain/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enuns;
+using Domain.Interfaces;
+
+namespace Domain.Models
+{
+    public class Workout : IWorkout
+    {
+        public string Name { get; set; }
+        public WorkoutTypeEnum Type { get; set; }
+        public WorkoutTimeTypeEnum TimeType { get; set; }
+        public int? Time { get; set; }
+        public int? Series { get; set; }
+        public int? Repetions { get; set; }
+        public int? Weight { get; set; }
+        public WorkoutRestTypeEnum RestType { get; set; }
+        public int? Rest { get; set; }
+    }
+}
