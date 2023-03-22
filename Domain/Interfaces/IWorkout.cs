b89c76cb719a047c7126195967071daefcbbba52
yuri-duque <yurithielmann83@gmail.com>
2023-03-21 23:16:10 -0300

    creating a workout interface

---
+++ Domain/Interfaces/IWorkout.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enuns;
+
+namespace Domain.Interfaces
+{
+    public interface IWorkout
+    {
+        string Name { get; set; }
+        WorkoutTypeEnum Type { get; set; }
+        WorkoutTimeTypeEnum TimeType { get; set; }
+        int? Time { get; set; }
+        int? Series { get; set; }
+        int? Repetions { get; set; }
+        int? Weight { get; set; }
+        WorkoutRestTypeEnum RestType { get; set; }
+        int? Rest { get; set; }
+    }
+}
